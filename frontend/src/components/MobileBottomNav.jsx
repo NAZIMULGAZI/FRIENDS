a88a1b9ec6bc7ce8e20bd07b0311667f24842a3d
Nazimul Gazi <nazimulgazi6@gmail.com>
2025-08-05 01:51:44 +0530

    change

--- frontend/src/components/MobileBottomNav.jsx
+++ frontend/src/components/MobileBottomNav.jsx
@@ -11,6 +11,7 @@ import {
 } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
  
 const MobileBottomNav = () => {
   const navigate = useNavigate();
