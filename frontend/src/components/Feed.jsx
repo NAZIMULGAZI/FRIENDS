d0ca749a03deeea94ab7922a97058fc52b744286
Nazimul Gazi <nazimulgazi6@gmail.com>
2025-08-14 19:14:53 +0530

    commit

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -3,9 +3,11 @@ import Posts from './Posts'
 
 const Feed = () => {
   return (
-    <div className="flex-1 my-8 flex flex-col items-center bg-[#B83218] py-4">
-      <div className="w-full max-w-2xl">
-        <Posts />
+    <div className="flex-1 h-screen flex flex-col bg-white">
+      <div className="flex-1 overflow-y-auto flex justify-center">
+        <div className="w-full max-w-2xl flex flex-col justify-center min-h-full">
+          <Posts />
+        </div>
       </div>
     </div>
   )
