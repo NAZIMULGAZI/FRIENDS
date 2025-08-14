6c35bf4ca61ac2a9321294cd3f47b14fafb38920
Nazimul Gazi <nazimulgazi6@gmail.com>
2025-08-14 19:48:13 +0530

    commit

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,19 +1,36 @@
-const Home = () => {
-    useGetAllPost();
-    useGetSuggestedUsers();
-    
-    return (
-        <div className="flex h-screen">
-            {/* Main feed area */}
-            <div className="flex-grow overflow-y-auto">
-                <Feed />
-                <Outlet />
-            </div>
+import React from "react";
+import LeftSidebar from "./LeftSidebar";
+import Feed from "./Feed";
+import RightSidebar from "./RightSidebar";
+import { Outlet } from "react-router-dom";
+import useGetAllPost from "@/hooks/useGetAllPost";
+import useGetSuggestedUsers from "@/hooks/useGetSuggestedUsers";
 
-            {/* Right sidebar */}
-            <div className="w-80 overflow-y-auto flex-shrink-0 border-l ">
-                <RightSidebar />
-            </div>
+const Home = () => {
+  useGetAllPost();
+  useGetSuggestedUsers();
+
+  return (
+    <div className="flex h-screen">
+      {/* Left Sidebar (fixed) */}
+      <div className="hidden md:block w-64">
+        <LeftSidebar />
+      </div>
+
+      {/* Feed (scrollable) */}
+      <div className="flex-grow overflow-y-auto">
+        <Feed />
+        <Outlet />
+      </div>
+
+      {/* Right Sidebar (fixed) */}
+      <div className="hidden lg:block w-80">
+        <div className="fixed right-0 top-0 h-screen w-80 border-l bg-white ">
+          <RightSidebar />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
+
+export default Home;
