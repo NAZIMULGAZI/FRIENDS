d6cd99122a729afe8ddf353b97be3fc7c73302d2
Nazimul Gazi <nazimulgazi6@gmail.com>
2025-08-14 19:28:07 +0530

    commit

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -4,7 +4,7 @@ import Posts from './Posts'
 const Feed = () => {
   return (
     <div className="flex-1 h-screen flex flex-col bg-white">
-      <div className="flex-1 overflow-y-auto flex justify-center">
+      <div className="flex-1 overflow-y-auto hide-scrollbar flex justify-center">
         <div className="w-full max-w-2xl flex flex-col justify-center min-h-full py-4">
           <Posts />
         </div>
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,26 +1,3 @@
-// import React from 'react'
-// import Feed from './Feed'
-// import { Outlet } from 'react-router-dom'
-// import RightSidebar from './RightSidebar'
-// import useGetAllPost from '@/hooks/useGetAllPost'
-// import useGetSuggestedUsers from '@/hooks/useGetSuggestedUsers'
-
-// const Home = () => {
-//     useGetAllPost();
-//     useGetSuggestedUsers();
-//     return (
-//         <div className='flex h-screenw-full'>
-//             <div className='flex-grow overflow-y-auto'>
-//                 <Feed />
-//                 <Outlet />
-//             </div>
-//             <RightSidebar />
-//         </div>
-//     )
-// }
-
-// export default Home
- 
 import React from "react";
 import LeftSidebar from "./LeftSidebar";
 import Feed from "./Feed";
@@ -35,22 +12,22 @@ const Home = () => {
 
   return (
     <div className="flex h-screen">
-      {/* Fixed Left Sidebar */}
+      {/* Fixed Left Sidebar with hidden scrollbar */}
       <div className="hidden md:block w-64">
-        <div className="fixed top-0 left-0 h-screen w-64 bg-white border-r border-gray-200">
+        <div className="fixed top-0 left-0 h-screen w-64 bg-white border-r border-gray-200 overflow-y-auto hide-scrollbar">
           <LeftSidebar />
         </div>
       </div>
 
-      {/* Feed (independent scroll) */}
+      {/* Feed (hidden scrollbar) */}
       <div className="flex-grow ml-64 mr-80">
         <Feed />
         <Outlet />
       </div>
 
-      {/* Fixed Right Sidebar */}
+      {/* Fixed Right Sidebar with hidden scrollbar */}
       <div className="hidden lg:block w-80">
-        <div className="fixed top-0 right-0 h-screen w-80 bg-white border-l border-gray-200 overflow-y-auto">
+        <div className="fixed top-0 right-0 h-screen w-80 bg-white border-l border-gray-200 overflow-y-auto hide-scrollbar">
           <RightSidebar />
         </div>
       </div>
