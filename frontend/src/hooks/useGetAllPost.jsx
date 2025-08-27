91ef54c46307954d6030cf77fb619dab583218c4
Nazimul Gazi <nazimulgazi6@gmail.com>
2025-08-28 01:55:21 +0530

    commit

--- frontend/src/hooks/useGetAllPost.jsx
+++ frontend/src/hooks/useGetAllPost.jsx
@@ -9,7 +9,12 @@ const useGetAllPost = () => {
     useEffect(() => {
         const fetchAllPost = async () => {
             try {
-                const res = await axios.get('https://friends-13.onrender.com/api/v1/post/all', { withCredentials: true });
+                const res = await axios.get(
+  "https://friends-13.onrender.com/api/v1/post/all",
+  {
+    headers: {
+      Authorization: `Bearer ${token}`,  // 👈 attach token
+    } , withCredentials: true });
                 if (res.data.success) { 
                     console.log(res.data.posts);
                     dispatch(setPosts(res.data.posts));
