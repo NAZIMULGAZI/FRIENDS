0d61158b73d57d614fe884894b66562c95a202d5
Nazimul Gazi <nazimulgazi6@gmail.com>
2025-08-28 02:45:50 +0530

    commit

--- frontend/src/components/CreatePost.jsx
+++ frontend/src/components/CreatePost.jsx
@@ -41,7 +41,7 @@ const CreatePost = ({ open, setOpen }) => {
     try {
       setLoading(true);
       const res = await axios.post(
-        "https://friends-13.onrender.com/api/v1/post/addpost",
+        "/post/addpost",
         formData,
         {
           headers: {
