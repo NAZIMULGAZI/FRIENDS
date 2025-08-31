d14903a6eaba6368ed398c97363e1576d6a10e26
Nazimul Gazi <nazimulgazi6@gmail.com>
2025-09-01 01:50:36 +0530

    commit

--- frontend/src/hooks/useGetSuggestedUsers.jsx
+++ frontend/src/hooks/useGetSuggestedUsers.jsx
@@ -1,52 +1,23 @@
-// import { setSuggestedUsers } from "@/redux/authSlice";
-// import axios from "axios";
-// import { useEffect } from "react";
-// import { useDispatch } from "react-redux";
-
-
-// const useGetSuggestedUsers = () => {
-//     const dispatch = useDispatch();
-//     useEffect(() => {
-//         const fetchSuggestedUsers = async () => {
-//             try {
-//                 const res = await axios.get('https://friends-l46d.onrender.com/api/v1/user/suggested', { withCredentials: true });
-//                 if (res.data.success) { 
-//                     dispatch(setSuggestedUsers(res.data.users));
-//                 }
-//             } catch (error) {
-//                 console.log(error);
-//             }
-//         }
-//         fetchSuggestedUsers();
-//     }, []);
-// };
-// export default useGetSuggestedUsers;
-
 import { setSuggestedUsers } from "@/redux/authSlice";
 import axios from "axios";
-import { useEffect, useCallback } from "react";
+import { useEffect } from "react";
 import { useDispatch } from "react-redux";
+
 
 const useGetSuggestedUsers = () => {
     const dispatch = useDispatch();
-
-    const fetchSuggestedUsers = useCallback(async () => {
-        try {
-            const res = await axios.get(
-                "https://friends-13.onrender.com/api/v1/user/suggested",
-                { withCredentials: true }
-            );
-            if (res.data && res.data.success) {
-                dispatch(setSuggestedUsers(res.data.users));
-            }
-        } catch (error) {
-            console.error("Failed to fetch suggested users:", error);
-        }
-    }, [dispatch]);
-
     useEffect(() => {
+        const fetchSuggestedUsers = async () => {
+            try {
+                const res = await axios.get(' https://friends-13.onrender.com/api/v1/user/suggested', { withCredentials: true });
+                if (res.data.success) { 
+                    dispatch(setSuggestedUsers(res.data.users));
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
         fetchSuggestedUsers();
-    }, [fetchSuggestedUsers]);
+    }, []);
 };
-
 export default useGetSuggestedUsers;
